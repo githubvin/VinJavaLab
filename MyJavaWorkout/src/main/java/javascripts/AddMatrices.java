ec99117a26dc407a5f16aa16cfc098d79e156dba
vinvi <vinvi@VIN-PC>
2020-05-30 00:32:00 +0530

    Made some changed in Add matrix to accommodate any dimension matrix.

--- MyJavaWorkout/src/main/java/javascripts/AddMatrices.java
+++ MyJavaWorkout/src/main/java/javascripts/AddMatrices.java
@@ -28,18 +28,14 @@ public class AddMatrices {
 		
 		System.out.println("After Adding the matrices");
 		for (int i = 0; i < mat1.length; i++) { 
-			for (int j = 0; j < mat2.length; j++) { 
+			for (int j = 0; j < mat2[0].length; j++) { 
 				 res[i][j] = mat1[i][j] + mat2[i][j]; 
 				 System.out.print(res[i][j] + " ");
 			} 
 			System.out.println();
 		}
 
-		
-		
-		
-		
-		
+				
 		
 	}
 
